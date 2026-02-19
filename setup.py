6d9ae725c6b1f427a57a6f161af946ad49ef4e83
Jon Durbin <jon@jondurbin.com>
2026-02-19 09:13:44 +0000

    foo.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md"))
 
 setup(
     name="cllmv",
-    version="0.1.0",
+    version="0.1.1",
     description="Chutes fast LLM verification.",
     long_description=long_description,
     long_description_content_type="text/markdown",
